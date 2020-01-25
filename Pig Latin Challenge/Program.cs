dd20d8610129dbc6950c72db2839d66cb6905ced
Michael <Michael@acdmail.com>
2020-01-25 18:17:00 -0500

    first commit

---
+++ Pig Latin Challenge/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+
+namespace Pig_Latin_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's try some Pig Latin!");
+            PigLatinTranslator();
+        }
+        public static void PigLatinTranslator()
+        {
+            string validInput = "y";
+            while (validInput == "y")
+            {
+                //prompt
+                Console.Write("\nEnter a word to translate to Pig Latin: ");
+
+                //input
+                string word = Console.ReadLine();
+                string wordLower = word.ToLower();
+                char firstCharacter = wordLower[0];
+
+                //lower case letter validation
+                if (Char.IsLower(firstCharacter) == true)
+                {
+                    int firstVowelPosition = 0;
+                    StringBuilder firstLetters = new StringBuilder("");
+                    StringBuilder newWord = new StringBuilder("");
+
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (ContainsVowel(word[i]) == true)
+                        {
+                            firstVowelPosition = i;
+                            firstLetters.Append(word.Substring(0, firstVowelPosition));
+                            break;
+                        }
+                    }
+
+                    newWord.Append(word.Substring(firstVowelPosition) + firstLetters + "ay");
+                    Console.WriteLine($"{newWord} is your new word");
+                    YesOrNo();
+                }
+            }
+        }
+        public static bool ContainsVowel(char c)
+        {
+            bool result = false;
+            string vowels = "aeiouAEIOU";
+            if (vowels.Contains(c))
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static void YesOrNo()
+        {
+            string userContinue = "";
+            while (userContinue != "y" && userContinue != "n")
+            {
+                Console.WriteLine("\nWould you like to translate another word? y/n?");
+                userContinue = Console.ReadLine().ToLower();
+
+                if (userContinue == "n")
+                {
+                    Console.WriteLine("Okay!!");
+                }
+                else if (userContinue == "y")
+                {
+                    PigLatinTranslator();
+                }
+            }
+        }
+    }
+}
